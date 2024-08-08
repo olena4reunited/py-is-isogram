cc3ba0f4810056b5c347d409cdbc1bcfb6e7c771
Olena Cherneha <166736208+olena4reunited@users.noreply.github.com>
2024-08-08 15:10:29 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,26 +3,26 @@ import pytest
 from app.main import is_isogram
 
 
-def should_return_true_when_playgrounds():
-    assert is_isogram("playgrounds") == True
+def test_should_return_true_when_playgrounds() -> None:
+    assert is_isogram("playgrounds")
 
 
-def should_return_true_when_empty_string():
-    assert is_isogram("") == True
+def test_should_return_true_when_empty_string() -> None:
+    assert is_isogram("")
 
 
-def should_return_true_when_isogram():
-    assert is_isogram("uncopyrightable") == True
+def test_should_return_true_when_isogram() -> None:
+    assert is_isogram("uncopyrightable")
 
 
-def should_return_false_when_letters_repeat():
-    assert is_isogram("look") == False
+def test_should_return_false_when_letters_repeat() -> None:
+    assert not is_isogram("look")
 
 
-def should_return_false_ignoring_the_case():
-    assert is_isogram("Adam") == False
+def test_should_return_false_ignoring_the_case() -> None:
+    assert not is_isogram("Adam")
 
 
-def test_should_raise_attribute_error() -> None:
+def test_test_should_raise_attribute_error() -> None:
     with pytest.raises(AttributeError):
         is_isogram(123)
