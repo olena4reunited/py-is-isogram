4132effbc602c6f229f28e995f9672baf5eb4a68
Olena Cherneha <166736208+olena4reunited@users.noreply.github.com>
2024-08-08 14:46:22 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected_result",
+    [
+        ("playgrounds", True),
+        ("", True),
+        ("uncopyrightable", True),
+        ("ambidextrously", True),
+        ("look", False),
+        ("Adam", False)
+    ]
+)
+def should_return_correct_answer(word: str, expected_result: bool) -> None:
+    assert is_isogram(word) == expected_result
